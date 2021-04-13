50d0d467668694a8bf286950c57bfc8b6e5eb72d
MK <godekd3133@naver.com>
2021-04-13 20:43:22 +0900

    LaunchScene 완료

---
+++ Assets/Scripts/LaunchScene/LaunchScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LaunchScene : MonoBehaviour
+{
+    public Button startButton;
+
+    int MaxStage;
+
+    void Start()
+    {
+        MaxStage = PlayerPrefs.GetInt("MaxStage", 1);
+
+        //임시코드
+        MaxStage = 2;
+
+        startButton.onClick.AddListener(OnClickStartButton);
+    }
+
+    public void OnClickStartButton()
+    {
+        if (MaxStage == 1)
+            SceneManager.LoadScene("1stScene");
+
+        else if (MaxStage == 2)
+            SceneManager.LoadScene("2ndScene");
+
+        else if (MaxStage == 3)
+            SceneManager.LoadScene("3rdScene");
+
+        else if (MaxStage == 4)
+            SceneManager.LoadScene("4thScene");
+    }
+
+
+}
