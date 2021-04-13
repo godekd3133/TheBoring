ae0a9930b7455008dbc3a360b0ecb6bb45d62340
olivesch <olivesch@naver.com>
2021-04-13 22:15:49 +0900

    game_307 업데이트

---
+++ Assets/Scripts/Game_307.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_307 : Page
+{
+    [SerializeField] GameObject guideObj = null;
+
+
+    public void ShowGuide(bool isActive)
+    {
+        guideObj.SetActive(isActive);
+    }
+}
