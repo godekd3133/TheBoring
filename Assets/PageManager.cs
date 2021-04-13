6b759eb884d8bde24e2b31367144bbf66f84f205
olivesch <olivesch@naver.com>
2021-04-14 00:56:46 +0900

    page 툴 완성

--- Assets/PageManager.cs
+++ Assets/PageManager.cs
@@ -2,7 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PageManager : MonoBehaviour
+public abstract class GlobalInstance<T> : MonoBehaviour where T : GlobalInstance<T>
 {
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<T>();
+            return instance;
+        }
+    }
+    private static T instance = null;
+}
+
+
+public class PageManager : GlobalInstance<PageManager>
+{
+    public Transform canvas;
+    public int currentPageIndex = 1;
+    public int bookMarkIndex = 0;
+    public int bookMarkCount = 4;
+
+
+    public int problemPage = 1;
+    public int macaronCount = 5;
+    public int getStarCount = 0;
+
+    public void GainPage()
+    {
+        var oldTf = canvas.GetChild(bookMarkCount + (currentPageIndex) - 1);
+        oldTf.gameObject.SetActive(false);
+
+        var tf = canvas.GetChild(bookMarkCount + (--currentPageIndex) - 1);
+        tf.gameObject.SetActive(true);
+        tf.GetComponent<Page>().ResetPage();
+
+    }
+    public void NextPage()
+    {
+        var oldTf = canvas.GetChild(bookMarkCount + (currentPageIndex) - 1);
+        oldTf.gameObject.SetActive(false);
+
+        var tf = canvas.GetChild(bookMarkCount + (++currentPageIndex) - 1);
+        tf.gameObject.SetActive(true);
+        tf.GetComponent<Page>().ResetPage();
+    }
 
 }
---
+++ Assets/Scripts/Game_308.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TapImageGame : Page
+{
+    public Text pageText = null;
+    public Text macaronCount = null;
+    public Text starCount = null;
+
+    public GameObject hintObj = null;
+    public GameObject realHintObj = null;
+    public GameObject nextPageObj = null;
+
+    public Button[] buttons = null;
+
+    bool isActiveHint = false;
+
+    bool isCardSelect = false;
+
+    public override void ResetPage()
+    {
+        starCount.text = PageManager.Instance.getStarCount.ToString();
+        macaronCount.text = PageManager.Instance.macaronCount.ToString();
+        pageText.text = PageManager.Instance.problemPage.ToString() + " of 10";
+    }
+
+    private void Start()
+    {
+        foreach(var button in buttons)
+        {
+            button.onClick.AddListener(() => {
+                if (isCardSelect) return;
+                isCardSelect = true;
+                nextPageObj.SetActive(true);
+                button.gameObject.SetActive(false); });
+        }
+    }
+
+    public void ShowHint(bool isActive)
+    {
+        if (isActiveHint)
+            return;
+
+        hintObj.SetActive(isActive);
+    }
+    public void ShowHintObj(bool isActive)
+    {
+        isActiveHint = true;
+
+        PageManager.Instance.macaronCount -= 1;
+        macaronCount.text = PageManager.Instance.macaronCount.ToString();
+
+        realHintObj.SetActive(isActive);
+    }
+
+    public void GetStar(int count)
+    {
+        if (isCardSelect)
+            return;
+        PageManager.Instance.getStarCount += count;
+        starCount.text = PageManager.Instance.getStarCount.ToString();
+    }
+}
+
+
+public class Game_308 : TapImageGame
+{
+}
