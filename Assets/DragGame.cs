437f40ee9330b4095b0f6fbdad42177823dbdfd3
olivesch <olivesch@naver.com>
2021-04-15 22:45:16 +0900

    no message

---
+++ Assets/DragGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragGame : Page
+{
+    public Transform bingoBoard = null;
+    public RectTransform rectTp = null;
+    public List<Text> listText = null;
+    public string text = null;
+
+    public DragObject dragObject = null;
+
+    private void Start()
+    {
+        string[] chars = text.Split(',');
+        listText = bingoBoard.GetComponentsInChildren<Text>().ToList();
+        for (int i = 0; i < listText.Count; i++)
+        {
+            listText[i].text = chars[i];
+        }
+
+    }
+
+
+}
