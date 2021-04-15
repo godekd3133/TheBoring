f9332de4bd180180dc686f0a5a9305270224f993
olivesch <olivesch@naver.com>
2021-04-15 23:27:13 +0900

    no message

--- Assets/PageManager.cs
+++ Assets/PageManager.cs
@@ -23,6 +23,7 @@ public class PageManager : GlobalInstance<PageManager>
     public int currentPageIndex = 1;
     public int bookMarkIndex = 0;
     public int bookMarkCount = 4;
+    public int[] bookMarkChangeIndex;
 
 
     public int problemPage = 0;
@@ -31,22 +32,36 @@ public class PageManager : GlobalInstance<PageManager>
 
     public void PrevPage()
     {
-        var oldTf = canvas.GetChild(bookMarkCount + (currentPageIndex) - 1);
+        var oldTf = canvas.GetChild(bookMarkCount + (currentPageIndex));
         oldTf.gameObject.SetActive(false);
 
-        var tf = canvas.GetChild(bookMarkCount + (--currentPageIndex) - 1);
+        var tf = canvas.GetChild(bookMarkCount + (--currentPageIndex));
         tf.gameObject.SetActive(true);
         tf.GetComponent<Page>().OnResetPage();
 
     }
     public void NextPage()
     {
-        var oldTf = canvas.GetChild(bookMarkCount + (currentPageIndex) - 1);
+        var oldTf = canvas.GetChild(bookMarkCount + (currentPageIndex));
         oldTf.gameObject.SetActive(false);
 
-        var tf = canvas.GetChild(bookMarkCount + (++currentPageIndex) - 1);
+        var tf = canvas.GetChild(bookMarkCount + (++currentPageIndex));
         tf.gameObject.SetActive(true);
         tf.GetComponent<Page>().OnResetPage();
+
+        for(int i = 0; i < bookMarkChangeIndex.Length; i++)
+        {
+            if(bookMarkChangeIndex[i] == (bookMarkCount + currentPageIndex))
+            {
+                var bookTf = canvas.GetChild(i);
+                bookTf.gameObject.SetActive(true);
+
+                var oldBookTf = canvas.GetChild(i - 1);
+                oldBookTf.gameObject.SetActive(false);
+            }
+        }
+
+
     }
 
 }
