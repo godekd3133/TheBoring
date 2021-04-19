6470367aaab09907aa66ec437899f7197e755622
MinkyuKim <godekd3133@naver.com>
2021-04-19 23:15:20 +0900

    3차시 완성

---
+++ Assets/Scripts/Write_411.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Write_411 : Page
+{
+    public List<Image> stars;
+
+    public Sprite nonStar;
+    public Sprite star;
+
+    public GameObject NextButton;
+
+    private void Start()
+    {
+        NextButton.SetActive(false);
+    }
+
+    public override void OnResetPage()
+    {
+        base.OnResetPage();
+    }
+
+    public void Star(int cnt)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (i < cnt)
+                stars[i].sprite = star;
+            else
+                stars[i].sprite = nonStar;
+            NextButton.SetActive(true);
+        }
+
+    }
+
+}
