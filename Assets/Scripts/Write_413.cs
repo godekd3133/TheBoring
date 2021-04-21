68c3b01c4ece4243f0431c5306d4032937633257
MK <godekd3133@naver.com>
2021-04-21 21:56:45 +0900

    write 413

--- Assets/Scripts/Write_413.cs
+++ Assets/Scripts/Write_413.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Write_413 : MonoBehaviour
+public class Write_413 : Page
 {
+    public float timeLimit;
+    public Image Gauge;
+    public GameObject gameOver;
+
+    public InputField InputField;
+    public string correctAnswer;
+
+    public GameObject Fix;
+    public GameObject ClearMessage;
+
+    public bool success;
+
+    [HideInInspector] public float currentTimer;
+
+    public void OnSuccess()
+    {
+        Fix.SetActive(true);
+        success = true;
+    }
+
+    public override void OnResetPage()
+    {
+        base.OnResetPage();
+        currentTimer = timeLimit;
+        success = false;
+        InputField.text = "";
+        Fix.SetActive(false);
+        gameOver.SetActive(false);
+        ClearMessage.SetActive(false);
+        InputField.interactable = true;
+    }
+
+    public void ChkInput()
+    {
+        if (InputField.text == correctAnswer)
+        {
+            InputField.interactable = false;
+            ClearMessage.SetActive(true);
+        }
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        OnResetPage();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (success == false)
+        {
+            if (currentTimer < 0f)
+            {
+                gameOver.SetActive(true);
+            }
+            else
+                currentTimer -= Time.deltaTime;
+
+            Gauge.fillAmount = currentTimer / timeLimit;
+        }
+    }
+
+    public void OnHintButtonDown()
+    {
+
     }
 }
