c9ea77d2d9f2b37c284f5a86319a58136277cf72
olivesch <olivesch@naver.com>
2021-04-22 00:24:50 +0900

    no message

---
+++ Assets/Scripts/Game_4/Game_4.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game_4 : Page
+{
+    public Transform dragImageP;
+    public Transform dropImageP;
+    
+    public int[] dropIndex;
+
+    public Drag_4 dragObject;
+
+    public GraphicRaycaster gr;
+
+    public int selectIndex = 0;
+    public int remainNumber = 20;
+
+
+    public void CheckWin()
+    {
+        bool isWin = true;
+        for(int i = 0; i < dropIndex.Length; i++)
+        {
+            if (dropIndex[i] != -1)
+                isWin = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Game_4/Game_DragSlot.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Game_DragSlot : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public Game_4 game;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        for(int i = 0; i < game.dragImageP.childCount; i++)
+        {
+            if (this == game.dragImageP.GetChild(i).GetComponent<Game_DragSlot>())
+                game.selectIndex = i + 1;
+        }
+
+        game.dragObject.gameObject.SetActive(true);
+        var image = game.dragObject.transform.GetComponent<Image>();
+        image.sprite = transform.GetComponent<Image>().sprite;
+        image.color = new Color(1, 1, 1, 0.5f);
+        game.dragObject.transform.GetComponent<RectTransform>().sizeDelta = (transform.GetComponent<RectTransform>().sizeDelta * 0.9f);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        game.dragObject.gameObject.SetActive(false);
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        game.gr.Raycast(eventData, results);
+
+        if (results.Count <= 0)
+            game.dragObject.gameObject.SetActive(false);
+        
+
+        int findIndex = 0;
+        for (int i = 0; i < results.Count; i++)
+        {
+            if (results[i].gameObject.GetComponent<Game_DropSlot>())
+                findIndex = i;
+        }
+
+        for (int i = 0; i < game.dropImageP.childCount; i++)
+        {
+            if (results[findIndex].gameObject.GetComponent<Game_DropSlot>() == 
+                game.dropImageP.GetChild(i).GetComponent<Game_DropSlot>())
+            {
+                if(game.dropIndex[i] == game.selectIndex)
+                {
+                    results[findIndex].gameObject.GetComponent<Game_DropSlot>().transform.GetComponent<Image>().sprite = game.dragObject.transform.GetComponent<Image>().sprite;
+                    game.dropIndex[i] = -1;
+                    game.CheckWin();
+                }
+                else
+                {
+                    game.remainNumber--;
+                }
+
+
+            }
+        }
+
+
+        game.dragObject.gameObject.SetActive(false);
+    }
+}
